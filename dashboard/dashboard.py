5a1603394a908e37a9fb41e90998ba2a09b4091d
hyeonshin <nashrullah1807@gmail.com>
2024-12-01 20:42:33 +0700

    edited

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,9 +5,10 @@ import streamlit as st
 from babel.numbers import format_currency
 import os
 st.write("Current working directory:", os.getcwd())
+st.write("Files in working directory:", os.listdir(os.getcwd()))
 sns.set(style='dark')
 
-all_df = pd.read_csv("/dashboard/main_data.csv")
+all_df = pd.read_csv("main_data.csv")
 
 st.header('Air Quality Tiantan Station Dashboard:sparkles:')
 
